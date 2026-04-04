a93f3a5af017f83a785a36c3066da60ed77cf1e4
w4daka <w4daka999@gmail.com>
2026-04-04 20:22:36 +0900

    feat crateを導入するところまでやった

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Gusee the number!");
-    println!("please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
 
+    println!("Please input your guess.");
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess);
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
     println!("You guessued: {guess}");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
